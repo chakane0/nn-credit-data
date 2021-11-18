0e77fa9796bdb87420a254ebac55739a7c5a1724
chakane <cs499@njit.edu>
2021-11-18 18:13:32 -0500

    move readme from source files to Tests

--- Source-Files/neuralNetwork.py
+++ Source-Files/neuralNetwork.py
@@ -3,8 +3,8 @@ import numpy as np
 df_train = pd.DataFrame(pd.read_excel("credit-data.xlsx")) # 999 rows x 21 columns -> (999, 21)
 df_test = pd.DataFrame(pd.read_excel("test-data.xlsx")) # 20 rows x 21 columns -> (20, 21)
 
-# convert our dataframe into a matrix which represents our vector inputs
 """ 
+convert our dataframe into a matrix which represents our vector inputs
 i.e)
 
 [[ 12  48  32 ... 191 201   2]
@@ -18,5 +18,3 @@ i.e)
 """
 training_input_matrix = df_train.to_numpy() 
 training_output_matrix = df_test.to_numpy()
-print(training_input_matrix)
-print(training_output_matrix)
